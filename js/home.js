5821e434ab41691786106f7438c319512e1caeec
ikbaldemirci <muhammed_5m@hotmail.com>
2024-02-02 03:32:29 +0300

    iletisim.html done i guess

--- js/home.js
+++ js/home.js
@@ -4,7 +4,8 @@
 let stars = document.getElementById('stars');
 // let moon = document.getElementById('moon');
 let mountains_behind = document.getElementById('mountains_behind');
-let text = document.getElementById('text');
+//let text = document.getElementById('text');
+let logo = document.getElementById('logo');
 let btn = document.getElementById('btn');
 let mountains_front = document.getElementById('mountains_front');
 //let header = document.querySelector('header');
@@ -16,6 +17,8 @@ window.addEventListener('scroll', function(){
     // moon.style.top = value * 1.05 + 'px';
     mountains_behind.style.top = value * 0.5 + 'px';
     mountains_front.style.top = value * 0 + 'px';
+    //logo.style.marginRight = value * 5 + 'px';
+    //logo.style.marginTop = value * 5 + 'px';
     text.style.marginRight = value * 5 + 'px';
     text.style.marginTop = value * 1.5 + 'px';
     btn.style.marginTop = value * 1.5 + 'px';
