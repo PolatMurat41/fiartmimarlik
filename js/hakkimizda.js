21b59972b2f8348d74682ca3d6a3bdafd090f177
PolatMurat41 <sea.ase112@gmail.com>
2024-02-05 21:47:34 +0300

    about us continue

--- js/hakkimizda.js
+++ js/hakkimizda.js
@@ -45,6 +45,28 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+// bilgi kutusu
+
+
+var bilgiMetinleri = [
+    "Bu bilgi 1. kart ile ilgili.",
+    "Bu bilgi 2. kart ile ilgili.",
+    "Bu bilgi 3. kart ile ilgili.",
+    "Bu bilgi 4. kart ile ilgili."
+];
+
+
+function showbilgiBox(index) {
+    var bilgiBox = document.getElementById('bilgiBox');
+    bilgiBox.innerHTML = bilgiMetinleri[index - 1];
+    bilgiBox.style.display = 'block';
+}
+
+function hidebilgiBox() {
+    var bilgiBox = document.getElementById('bilgiBox');
+    bilgiBox.style.display = 'none';
+}
+// bilgi kutusu
 
 
 
