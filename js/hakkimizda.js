f6692e1e47149140f778471f0e75d0934f6d84f8
PolatMurat41 <sea.ase112@gmail.com>
2024-02-06 03:23:41 +0300

    about us continued 06/02/2024

--- js/hakkimizda.js
+++ js/hakkimizda.js
@@ -1,73 +1,70 @@
-
-
 //  HEADER SCROLL BEYAZ OLMASI
 document.addEventListener("DOMContentLoaded", function () {
-    var header = document.querySelector("header");
-    var scrollThreshold = 10; // Değişiklik için belirlenen scroll eşiği (pixel cinsinden)
+  var header = document.querySelector("header");
+  var scrollThreshold = 10; // Değişiklik için belirlenen scroll eşiği (pixel cinsinden)
 
-    function updateHeaderBackground() {
-        var scrollPosition = window.scrollY;
+  function updateHeaderBackground() {
+    var scrollPosition = window.scrollY;
 
-        if (scrollPosition > scrollThreshold) {
-            header.classList.add("scrolled");
-        } else {
-            header.classList.remove("scrolled");
-        }
+    if (scrollPosition > scrollThreshold) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
     }
+  }
 
-    window.addEventListener("scroll", updateHeaderBackground);
+  window.addEventListener("scroll", updateHeaderBackground);
 
-    var headerLinks = document.querySelectorAll("header ul li a");
+  var headerLinks = document.querySelectorAll("header ul li a");
 
-    function updateActivateLink(){
-        var currentURL = window.location.href;
+  function updateActivateLink() {
+    var currentURL = window.location.href;
 
-        headerLinks.forEach(function (link) {
-            var linkURL = link.href;
+    headerLinks.forEach(function (link) {
+      var linkURL = link.href;
 
-            if(currentURL.includes(linkURL) && !link.classList.contains("active")){
-                headerLinks.forEach(function (otherLink){
-                    otherLink.classList.remove("active")
-                });
-
-                link.classList.add("active")
-            }
+      if (currentURL.includes(linkURL) && !link.classList.contains("active")) {
+        headerLinks.forEach(function (otherLink) {
+          otherLink.classList.remove("active");
         });
-    }
 
-    if(window.location.pathname === '/hakkimizda.html'){
-        document.body.classList.add("hakkimizda-page");
-        headerLinks[2].classList.add("active");
-    }
+        link.classList.add("active");
+      }
+    });
+  }
 
-    window.addEventListener("load", updateActivateLink);
-    window.addEventListener("hashchange", updateActivateLink);
+  if (window.location.pathname === "/hakkimizda.html") {
+    document.body.classList.add("hakkimizda-page");
+    headerLinks[2].classList.add("active");
+  }
+
+  window.addEventListener("load", updateActivateLink);
+  window.addEventListener("hashchange", updateActivateLink);
 });
 
-
 // bilgi kutusu
-
-
-var bilgiMetinleri = [
-    "Bu bilgi 1. kart ile ilgili.",
-    "Bu bilgi 2. kart ile ilgili.",
-    "Bu bilgi 3. kart ile ilgili.",
-    "Bu bilgi 4. kart ile ilgili."
-];
-
-
 function showbilgiBox(index) {
-    var bilgiBox = document.getElementById('bilgiBox');
-    bilgiBox.innerHTML = bilgiMetinleri[index - 1];
+    // Tüm bilgikutusu div'lerini gizle
+    hideAllbilgiBox();
+
+    // Sadece ilgili bilgikutusu div'ini göster
+    var bilgiBox = document.getElementById('bilgiBox' + index);
     bilgiBox.style.display = 'block';
-}
+  }
 
-function hidebilgiBox() {
-    var bilgiBox = document.getElementById('bilgiBox');
-    bilgiBox.style.display = 'none';
-}
+  function hideAllbilgiBox() {
+    // Tüm bilgikutusu div'lerini gizle
+    for (var i = 1; i <=4; i++) {
+        var bilgiBox = document.getElementById('bilgiBox' + i);
+        bilgiBox.style.display = 'none';
+      }
+    }
+  
+    function hidebilgiBox() {
+      // Tüm bilgikutusu div'lerini gizle
+      for (var i = 1; i <= 4; i++) {
+        var bilgiBox = document.getElementById('bilgiBox' + i);
+        bilgiBox.style.display = 'none';
+      }
+    }
 // bilgi kutusu
-
-
-
-
